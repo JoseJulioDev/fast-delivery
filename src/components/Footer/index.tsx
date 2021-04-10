650a93f76a7b4481dd080af2181a159ac0c2362b
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2021-04-10 18:20:50 -0300

    app fast delivery

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg'
+
+function Footer() {
+    return(
+        <footer className="main-footer">
+            App desenvolvido por JoseJulioDev
+            <div className="footer-icons">
+                <a href="https://www.linkedin.com/in/jos%C3%A9-julio-umbelino-57264b209/" target="_new">
+                    <LinkedinIcon />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
